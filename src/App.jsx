2576c562736f9f8ee5e280564e1f0a6edd2fdb41
mohamed montaser <mastrocoding@gmail.com or muslim.mohamed.montaser@gmail.com>
2023-10-19 22:23:42 +0300

    responsive boost section

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Statistics />
-      {/* <Boost /> */}
+      <Boost />
       {/* <Footer /> */}
     </>
   );
--- src/components/Boost.jsx
+++ src/components/Boost.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BgBoostDesktop from "../images/bg-boost-desktop.svg";
+import BgBoostMobile from "../images/bg-boost-mobile.svg";
 
 export default function Boost() {
+  const [windowSize, setWindowSize] = useState(window.screen.availWidth);
+
+  useEffect(() => {
+    window.onresize = (e) => {
+      setWindowSize(e.currentTarget.screen.availWidth);
+    };
+  });
   return (
-    <div className="bg-[#3b3054] relative">
-      <img src={BgBoostDesktop} className="w-full" alt="bg boost desktop" />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h2 className="text-center text-white text-4xl font-semibold">
+    <div className="bg-[#3b3054] relative min-h-[200px]">
+      <img
+        src={windowSize > 652 ? BgBoostDesktop : BgBoostMobile}
+        className="w-full h-full inset-0 absolute"
+        alt="bg boost desktop"
+        srcSet="../images/bg-boost-mobile.svg 200vw"
+      />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full container-p">
+        <h2 className="text-center text-white text-4xl font-semibold w-full">
           Boost your links today
         </h2>
         <button className="btn--cyan mx-auto block mt-5 py-4 px-10">
